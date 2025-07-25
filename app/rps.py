739dd3bf0985f521f55026f19df7b6ea17430e03
j-s-6 <jms10261@stern.nyu.edu>
2025-07-24 20:30:33 -0400

    Update rps.py

--- app/rps.py
+++ app/rps.py
@@ -6,11 +6,13 @@ player_choice = input("Please select an option ('rock'), ('paper'), ('scissors')
 print("USER CHOSE:", player_choice)
 
 # todo - validation step
-
+ 
 import random
 
 VALID_OPTIONS = ["rock"],["paper"],["scissors"]
+
 computer_choice = random.choice(VALID_OPTIONS)
+print("COMPUTER CHOSE:", player_choice)
 
 # TODO: determine the winner
 
