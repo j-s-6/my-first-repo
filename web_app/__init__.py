7098ea82a4de4cae729952dfa242273661754a73
j-s-6 <jms10261@stern.nyu.edu>
2025-07-31 20:49:04 -0400

    Add about page and update templates
    Introduced an about.html template and updated the Flask route to render it. Improved formatting and links in home.html and results.html, and made minor code style adjustments in __init__.py for clarity and consistency.

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from app.rps import determine_winner
-app = Flask(__name__) #boilerplate line
 
-# WHEN SOMEONE VISITS HOME PAGE IN BROWSER,
-# TRIGGER THIS HOME FUNCTION AND RETURN
-# OR DISPLAY SOME VALUE
 
-@app.route('/') 
+app = Flask(__name__)
+
+# when someone visits the home page "/" in the browser,
+# trigger this "home" function, and return / display some value
+@app.route('/')
 def home():
     #return "THIS IS THE HOME PAGE"
-    return render_template("home.html") #pass in the name of a file that exists in the templates directory
+    return render_template("home.html") # pass in the name of a file that exists in the templates dir
 
-@app.route('/about') 
+
+# when someone visits "/about" in the browser, it will
+# trigger this "about" function, and return / display some
+@app.route('/about')
 def about():
-    return "THIS IS THE ABOUT PAGE"
+    #return "THIS IS THE ABOUT PAGE"
+    return render_template("about.html")
+
+
 
 @app.route('/results', methods=['POST'])
 def results():
-    # this is the data the user sent in a POST request when they submitted the form
-    print(request.form) #Like a dictionary with a key of "user_choice"
+    # this is the data the user sent in a POST request when they
+    # submitted the form
+    print(dict(request.form)) # like a dictionary with a key of "user_choice"
 
     # Get the user's choice from the form data
     user_choice = request.form.get('user_choice')
 
-    computer_choice = "rock"  # generate_random_choice()
+    computer_choice = "rock" # generate_random_choice()
 
     outcome = determine_winner(user_choice, computer_choice)
 
@@ -33,9 +40,8 @@ def results():
         user_choice=user_choice,
         computer_choice=computer_choice,
         outcome=outcome
-    )    
+    )
 
-#boilerplate line
 if __name__ == '__main__':
 
-    app.run(debug=True)    
+    app.run(debug=True)
