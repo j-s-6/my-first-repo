dcf8a9af5346657bfcdc1fd19f97027b969ba330
j-s-6 <jms10261@stern.nyu.edu>
2025-07-31 20:44:46 -0400

    Add Rock Paper Scissors game web interface
    Implemented Flask routes for home, about, and results pages. Added HTML templates for the home page with a game form and results page to display the outcome. Integrated game logic using determine_winner and handled user input via POST requests.

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 
+from app.rps import determine_winner
 app = Flask(__name__) #boilerplate line
 
 # WHEN SOMEONE VISITS HOME PAGE IN BROWSER,
@@ -8,10 +9,33 @@ app = Flask(__name__) #boilerplate line
 
 @app.route('/') 
 def home():
-    return "THIS IS THE HOME PAGE"
+    #return "THIS IS THE HOME PAGE"
+    return render_template("home.html") #pass in the name of a file that exists in the templates directory
 
+@app.route('/about') 
+def about():
+    return "THIS IS THE ABOUT PAGE"
 
+@app.route('/results', methods=['POST'])
+def results():
+    # this is the data the user sent in a POST request when they submitted the form
+    print(request.form) #Like a dictionary with a key of "user_choice"
 
+    # Get the user's choice from the form data
+    user_choice = request.form.get('user_choice')
+
+    computer_choice = "rock"  # generate_random_choice()
+
+    outcome = determine_winner(user_choice, computer_choice)
+
+    return render_template("results.html",
+        x=5,
+        user_choice=user_choice,
+        computer_choice=computer_choice,
+        outcome=outcome
+    )    
+
+#boilerplate line
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    app.run(debug=True)    
